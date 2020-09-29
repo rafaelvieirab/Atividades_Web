b0fc376b4ccdc09519dc815aa2dcbe0a6666b997
rafaelvieirab <rafaelviebar@gmail.com>
2020-09-28 23:38:07 -0300

    feat/atividade4: criando view Register

--- Atividade4/web/src/main.js
+++ Atividade4/web/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import router from './router'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
-  render: function (h) { return h(App) },
+  router,
+  render: function (h) { return h(App) }
 }).$mount('#app')
---
+++ Atividade4/web/src/views/Register.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <form action="" method="POST">
+            <label for="name">Nome</label>
+            <input type="text" id="name" v-model="name" placeholder="Digite seu nome">
+
+            <label for="office">Cargo</label>
+            <input type="text" id="office" v-model="office" placeholder="Digite seu cargo">
+
+            <label for="birthDate">Data de Nascimento</label>
+            <input type="date" id="birthDate" v-model="birthDate" min="1950-01-01" max="2020-10-10">
+            
+            <label for="entryDate">Data de Entrada na Empresa</label>
+            <input type="date" id="entryDate" v-model="entryDate"  min="1950-01-01" max="2020-10-10">
+
+            <button type="submit" @click="addEmployee">Cadastrar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Register",
+    data() {
+        return {
+            name: "",
+            office: "",
+            birthDate:"",
+            entryDate:"",
+            baseURI: "http://localhost:8080/server/api"
+        }
+    },
+    methods: {
+        addEmployee: function() {
+            let employee = {
+                name = this.name,
+                office = this.office,
+                birthDate = this.birthDate,
+                entryDate = this.entryDate
+            }
+            this.$http.post(this.baseURI, employee).then(result => {
+
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
