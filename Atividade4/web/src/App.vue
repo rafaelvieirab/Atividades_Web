577368127898f25260a19b6a6f51af17d8c42749
rafaelvieirab <rafaelviebar@gmail.com>
2020-10-03 12:02:32 -0300

    feat/atividade4: finalizando o web

--- Atividade4/web/src/App.vue
+++ Atividade4/web/src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <Employees/>
 </template>
+
+<script>
+import Employees from './components/Employees.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Employees
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,18 +20,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- Atividade4/web/src/main.js
+++ Atividade4/web/src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
-import router from './router'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: function (h) { return h(App) }
+  render: function (h) { return h(App) },
 }).$mount('#app')
